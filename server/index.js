7459aefb9f3a84b996775d7d0e88af0f8682c4a9
Jackson Fisk <jacksonfisk@gmail.com>
2024-09-26 15:09:19 -0400

    HTTP communication working

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
-const express = require ('express')
+const express = require ('express');
 const path = require('path');
-const app = express()
+const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'http://localhost:8080'
+}));
+
 app.use(express.text());
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/static')));
 
 
-app.get('/home', (req, res) => {
-    res.send('Home endpoint hit!')
-})
+app.post('/submit-click-data', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+    console.log('test')
+});
 
 
-app.listen (3000, () => console.log('server ready'))
+app.listen (3001, () => console.log('Listening on port 3001'));
