361b9e1e3e607e234f3c673c608ea8878b080bcd
Jackson Fisk <jacksonfisk@gmail.com>
2024-10-03 14:44:39 -0400

    added routes and LR controller file

---
+++ server/src/controllers/LinearRegression.js
@@ -0,0 +1,15 @@
+
+//=======Intakes line and data points, returns new line after one LR iteration=======
+const handleClickData = (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+};
+//=======Initializes random starter line, for first iteration=======
+const createRandomLine = (req, res) => {
+    pass
+}
+
+
+module.exports = {
+    handleClickData
+};
